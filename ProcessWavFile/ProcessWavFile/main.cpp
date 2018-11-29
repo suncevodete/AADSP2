8abd6296e3730c3004511dc0af31e554b658804d
student <student@domain.local>
2018-11-29 17:14:07 +0100

    input gain added, made 5 channel output

--- ProcessWavFile/ProcessWavFile/main.cpp
+++ ProcessWavFile/ProcessWavFile/main.cpp
@@ -4,9 +4,20 @@
 #include "WAVheader.h"
 
 #define BLOCK_SIZE 16
-#define MAX_NUM_CHANNEL 8
+#define MAX_NUM_CHANNEL 4
+#define INPUT_GAIN  0.8912509381337456
 
 double sampleBuffer[MAX_NUM_CHANNEL][BLOCK_SIZE];
+double helpBuffer[MAX_NUM_CHANNEL][BLOCK_SIZE];
+
+void processing(double* help)
+{
+
+	//levi kanal ide u ls, l, c
+	//desni kanal ide u rs i r
+
+
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,8 +29,10 @@ int main(int argc, char* argv[])
 
 	// Init channel buffers
 	for(int i=0; i<MAX_NUM_CHANNEL; i++)
-		memset(&sampleBuffer[i],0,BLOCK_SIZE);
-
+	{
+		memset(&sampleBuffer[i], 0, BLOCK_SIZE);
+		memset(&helpBuffer[i], 0, BLOCK_SIZE);
+	}
 	// Open input and output wav files
 	//-------------------------------------------------
 	strcpy(WavInputName,argv[1]);
@@ -36,7 +49,7 @@ int main(int argc, char* argv[])
 	// Set up output WAV header
 	//-------------------------------------------------	
 	outputWAVhdr = inputWAVhdr;
-	outputWAVhdr.fmt.NumChannels = inputWAVhdr.fmt.NumChannels; // change number of channels
+	outputWAVhdr.fmt.NumChannels = MAX_NUM_CHANNEL + 1; // change number of channels
 
 	int oneChannelSubChunk2Size = inputWAVhdr.data.SubChunk2Size/inputWAVhdr.fmt.NumChannels;
 	int oneChannelByteRate = inputWAVhdr.fmt.ByteRate/inputWAVhdr.fmt.NumChannels;
@@ -50,7 +63,6 @@ int main(int argc, char* argv[])
 	// Write output WAV header to file
 	//-------------------------------------------------
 	WriteWavHeader(wav_out,outputWAVhdr);
-
 
 	// Processing loop
 	//-------------------------------------------------	
@@ -74,13 +86,30 @@ int main(int argc, char* argv[])
 				}
 			}
 
+			//oba kanala mnozim sa input_gain
+			for (int m = 0; m < BLOCK_SIZE; m++) 
+			{
+				sampleBuffer[0][m] = sampleBuffer[0][m] * INPUT_GAIN;
+				sampleBuffer[1][m] = sampleBuffer[1][m] * INPUT_GAIN;
+			}
+			
+			//od 2 kanala pravim 5kanalni izlaz
+			for (int m = 0; m < BLOCK_SIZE; m++) 
+			{
+				helpBuffer[0][m] = sampleBuffer[0][m];
+				helpBuffer[1][m] = sampleBuffer[0][m];
+				helpBuffer[2][m] = sampleBuffer[0][m];
+				helpBuffer[3][m] = sampleBuffer[1][m];
+				helpBuffer[4][m] = sampleBuffer[1][m];
+			}
+
 			//processing();
 
 			for(int j=0; j<BLOCK_SIZE; j++)
 			{
 				for(int k=0; k<outputWAVhdr.fmt.NumChannels; k++)
 				{	
-					sample = sampleBuffer[k][j] * SAMPLE_SCALE ;	// crude, non-rounding 			
+					sample = helpBuffer[k][j] * SAMPLE_SCALE ;	// crude, non-rounding 			
 					sample = sample >> (32 - inputWAVhdr.fmt.BitsPerSample);
 					fwrite(&sample, outputWAVhdr.fmt.BitsPerSample/8, 1, wav_out);		
 				}
